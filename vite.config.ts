7668cff67a4e7cc975acbed92a4a0f678b4ec98a
dwyers2 <dwyers@gmail.com>
2026-01-19 14:10:49 -0500

    try to get oauth to work

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 3000
+    port: 3000,
+        allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'nonexotically-nonphonetical-aidan.ngrok-free.dev'
+    ],
   }
 });
